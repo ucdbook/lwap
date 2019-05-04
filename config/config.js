e760135b1520d3c26addbd11a23b99a404ac1a55
ucdbook <12613tianyanrong@gmail.com>
2019-05-04 20:38:01 +0800

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 module.exports.db={
-  host     : '39.108.140.242',
-  port: '3306',
-  user     : 'www_linmedia_cn',
-  password : 'hj42HrXW8wDnYySD',
-  database : 'www_linmedia_cn'
+  host     : '',
+  port: '',
+  user     : '',
+  password : '',
+  database : ''
 };
